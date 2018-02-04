a075fb35fd51337f8ed30bc53e943df02c241a70
Lucien <18952904@qq.com>
2018-02-05 00:57:26 +0800

    0.0.4
    去掉了文件类型判断

--- File-Rename.py
+++ File-Rename.py
@@ -2,4 +2,4 @@ import os
 old = input('输入被替换的文件名:')
 new = input('这个文件名会被替换为:')
 for file in os.listdir('.'):
-    if file[-2: ] != 'py': os.rename(file, file.replace(old, new))
+    os.rename(file, file.replace(old, new))
