5e29420a1fbebcc5f64c390b6af8c2487d0bc276
Lucien <18952904@qq.com>
2018-02-05 01:00:19 +0800

    0.0.5
    优化了文件结构

--- File-Rename.py
+++ File-Rename.py
@@ -1,5 +1,4 @@
 import os
 old = input('输入被替换的文件名:')
 new = input('这个文件名会被替换为:')
-for file in os.listdir('.'):
-    os.rename(file, file.replace(old, new))
+for file in os.listdir('.'): os.rename(file, file.replace(old, new))
